f71be53cd74b20af69ee525caa9f1a35a98d6957
fath11 <fathimkamal@gmail.com>
2024-07-14 18:03:32 +0800

    implemented shaderedObjects

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as twgl from 'twgl.js'
 
+const icon = "data:image/svg+xml,%3Csvg%20width%3D%22129%22%20height%3D%22129%22%20viewBox%3D%220%200%20129%20129%22%20fill%3D%22none%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20d%3D%22M75%2067.1928H80V80.1928H75V67.1928Z%22%20fill%3D%22%239EE7F1%22%2F%3E%3Cpath%20d%3D%22M55.0731%2021.02L70.7722%2030.8701L60.6256%20108.129L43.464%20109.415L55.0731%2021.02Z%22%20fill%3D%22%239EE7F1%22%2F%3E%3Cpath%20d%3D%22M106.059%2046.3074L92.7209%2055.4334L53.5113%2034.7133L54.8128%2021.0354L106.059%2046.3074Z%22%20fill%3D%22%239EE7F1%22%2F%3E%3Cpath%20d%3D%22M102.549%2072.9834L89.2109%2079.3015L50.3659%2061.3893L51.6674%2047.7114L102.549%2072.9834Z%22%20fill%3D%22%239EE7F1%22%2F%3E%3Cpath%20d%3D%22M55.0731%2014L70.7722%2023.8501L60.6256%20101.109L43.464%20102.395L55.0731%2014Z%22%20fill%3D%22%23F19ED2%22%2F%3E%3Cpath%20d%3D%22M106.059%2039.2874L92.7209%2048.4134L53.5113%2027.6933L54.8128%2014.0154L106.059%2039.2874Z%22%20fill%3D%22%23F19ED2%22%2F%3E%3Cpath%20d%3D%22M102.549%2065.9634L89.2109%2072.2814L50.3659%2054.3693L51.6674%2040.6914L102.549%2065.9634Z%22%20fill%3D%22%23F19ED2%22%2F%3E%3Cpath%20d%3D%22M55.0731%2018.212L70.7722%2028.0621L60.6256%20105.321L43.464%20106.607L55.0731%2018.212Z%22%20fill%3D%22white%22%2F%3E%3Cpath%20d%3D%22M106.059%2043.4994L92.7209%2052.6254L53.5113%2031.9053L54.8128%2018.2274L106.059%2043.4994Z%22%20fill%3D%22white%22%2F%3E%3Cpath%20d%3D%22M102.549%2070.1754L89.2109%2076.4935L50.3659%2058.5813L51.6674%2044.9034L102.549%2070.1754Z%22%20fill%3D%22white%22%2F%3E%3Cpath%20d%3D%22M92%2070.1928H97V97.1928H92V70.1928Z%22%20fill%3D%22%23F1DF9E%22%2F%3E%3Cpath%20d%3D%22M69%2014.1928H72V29.1928H69V14.1928Z%22%20fill%3D%22%23F1DF9E%22%2F%3E%3Cpath%20d%3D%22M21%2018.1928H29V52.1928H21V18.1928Z%22%20fill%3D%22%23F1DF9E%22%2F%3E%3Cpath%20d%3D%22M37%2097.1928H47V115.193H37V97.1928Z%22%20fill%3D%22%23F19ED2%22%2F%3E%3Cpath%20d%3D%22M90%2018.1928H100V39.1928H90V18.1928Z%22%20fill%3D%22%23F19ED2%22%2F%3E%3Cpath%20d%3D%22M42%2039.1928H53V64.1928H42V39.1928Z%22%20fill%3D%22%239EE7F1%22%2F%3E%3C%2Fsvg%3E"
 const extensionId = "quakeFrag"
 
 var vertexShaderSource = `#version 300 es
@@ -57,10 +58,10 @@ class Skins {
         this.gl = renderer.gl
         const texture = this.gl.createTexture()
         this.gl.bindTexture(this.gl.TEXTURE_2D, texture)
-        this.gl.texParameteri(this.gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE)
-        this.gl.texParameteri(this.gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE)
-        this.gl.texParameteri(this.gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, this.gl.NEAREST)
-        this.gl.texParameteri(this.gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, this.gl.NEAREST)
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE)
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE)
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST)
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST)
         //gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
         //gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0,255,0,255]));
         this._texture = texture
@@ -103,33 +104,12 @@ class Skins {
 }
 //End of Skins, Please keep this comment if you wanna use this code :3
 
-const canvas = document.createElement("canvas")
-const gl = canvas.getContext("webgl2")
-if (!gl) {
-  console.error("HelloWorld: WebGL not supported")
-}
-
-const programInfo = twgl.createProgramInfo(gl, [vertexShaderSource, fragmentShaderSource])
-
-const positionBuffer = twgl.createBufferInfoFromArrays(gl, {
-  position: {
-      numComponents: 2,
-      data: [
-        -1, -1,
-        1, -1,
-        -1, 1,
-        -1, 1,
-        1, -1,
-        1, 1,
-      ]
-  },
-})
-
 class QuakeFragment {
   constructor(runtime) {
     window.TEST = this
 
     this.runtime = runtime
+    this.shaderedSprites = []
 
     this.initFormatMessage({
       extensionName: ["地震碎片", "Quake Fragmment"],
@@ -156,6 +136,11 @@ class QuakeFragment {
     return {
       id: "quakefrag",
       name: "Quake Fragment",
+      color1: "#6645F6",
+      color2: "#5237c5",
+      color3: "#6645F6",
+      blockIconURI: icon,
+      menuIconURI: icon,
       blocks: [
         {
           opcode: "applyShader",
@@ -179,26 +164,21 @@ class QuakeFragment {
   }
 
   applyShader({ SPRITE }, util) {
-    console.log(util)
     const target = this.__getTargetByIdOrName(SPRITE, util)
-
     const currentCostume = target.getCurrentCostume()
 
-    let skinId = this.runtime.renderer._nextSkinId++
-    let SkinsClass = new Skins(this.runtime)
-    let skin = new SkinsClass.SimpleSkin(
-      skinId,
-      this.runtime.renderer,
-    )
-    this.runtime.renderer._allSkins[skinId] = skin;
-    this.runtime.renderer.updateDrawableSkinId(target.drawableID, skinId)
-    skin.size = currentCostume.size
+    this.__check_shaderedObjects(target, currentCostume)
+    const shaderedObject = this.shaderedSprites[target.name]
+    const gl = shaderedObject.gl
+    const canvas = shaderedObject.canvas
+    const skin = shaderedObject.skin
+    const positionBuffer = shaderedObject.positionBuffer
 
     const img = new Image()
     img.src = currentCostume.asset.encodeDataURI()
 
-    canvas.width = this.runtime.stageWidth
-    canvas.height = this.runtime.stageHeight
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
     gl.viewport(0, 0, canvas.width, canvas.height)
 
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true)
@@ -215,6 +195,8 @@ class QuakeFragment {
       u_color: [Math.random(), Math.random(), Math.random(), 1],
       u_skin: texture
     }
+
+    const programInfo = twgl.createProgramInfo(gl, [vertexShaderSource, fragmentShaderSource])
   
     gl.useProgram(programInfo.program)
     twgl.setBuffersAndAttributes(gl, programInfo, positionBuffer)
@@ -225,7 +207,50 @@ class QuakeFragment {
     skin.setContent(canvas)
     this.runtime.requestRedraw()
   }
-  
+
+  __check_shaderedObjects(target, currentCostume) {
+
+    if (this.shaderedSprites[target.name]) return;
+
+    const canvas = document.createElement("canvas")
+    const gl = canvas.getContext("webgl2")
+    if (!gl) {
+      console.error(target.name, ": WebGL2 not supported!")
+    }
+
+    const positionBuffer = twgl.createBufferInfoFromArrays(gl, {
+      position: {
+          numComponents: 2,
+          data: [
+            -1, -1,
+            1, -1,
+            -1, 1,
+            -1, 1,
+            1, -1,
+            1, 1,
+          ]
+      },
+    })
+
+    let skinId = this.runtime.renderer._nextSkinId++
+    let SkinsClass = new Skins(this.runtime)
+    let skin = new SkinsClass.SimpleSkin(
+      skinId,
+      this.runtime.renderer,
+    )
+    this.runtime.renderer._allSkins[skinId] = skin;
+    this.runtime.renderer.updateDrawableSkinId(target.drawableID, skinId)
+    skin.size = currentCostume.size
+
+    this.shaderedSprites[target.name] = {
+      "drawableID": target.drawableID,
+      "skinId": skinId,
+      "skin": skin,
+      "canvas": canvas,
+      "gl": gl,
+      "positionBuffer": positionBuffer
+    }
+  }
 
   __getTargetByIdOrName(name, util) {
     if (name === '__myself__') return util.target
@@ -282,11 +307,16 @@ window.tempExt = {
     name: "quakefragment.extensionName",
     description: "quakefragment.description",
     extensionId: "quakefragment",
-    // iconURL: icon,
-    // insetIconURL: cover,
+    //iconURL: cover,
+    insetIconURL: icon,
     featured: true,
     disabled: false,
-    collaborator: "only for quakefragment test",
+    collaboratorList: [
+      {
+        collaborator: 'Fath11@Cocrea',
+        collaboratorURL: 'https://cocrea.world/@Fath11',
+      },
+    ],
   },
   l10n: {
     "zh-cn": {
