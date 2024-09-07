093a4753816df1400982a9dadea9881806aa9147
fath11 <fathimkamal@gmail.com>
2024-09-07 17:16:56 +0800

    renamed extension

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  name: 'quake-fragment', // Replace it with your extension name
+  name: 'better-quake', // Replace it with your extension name
   entry: ['src/index.ts', 'src/index.js'],
   target: ['esnext'],
   format: ['iife'],
   outDir: 'dist',
   banner: {
     // Replace it with your extension's metadata
-    js: `// Name: QuakeFragment
-// ID: quakefragment
-// Description: A better way to load fragment shaders for Gandi IDE.
+    js: `// Name: Better Quake
+// ID: betterquake
+// Description: Better shader loader.
 // By: Fath11
 // Original: Fath11
 // License: MPL-2.0
